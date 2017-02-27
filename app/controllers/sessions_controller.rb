2af636104dd461738af472c73a9c7c73277be099
dianedouglas <diane.douglas1@gmail.com>
2017-02-27 02:33:08 -0500

    Fix create user redirect to places page. Add menu links to application layout with banner.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to store_url, notice: "Logged out"
+    redirect_to '/', notice: "Logged out"
   end
 end
