96ff6d79e3da52e5e43aaaf1552d2c4c1a9a4368
dianedouglas <diane.douglas1@gmail.com>
2017-02-27 03:14:26 -0500

    Display the places that belong to the current user.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,7 @@ class PlacesController < ApplicationController
   # GET /places
   # GET /places.json
   def index
-    @places = Place.all
+    @places = Place.where(user_id: current_user.id)
   end
 
   # GET /places/1
