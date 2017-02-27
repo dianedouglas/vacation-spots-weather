b14dea6310bdbc843710134ed2aadc93f07baa27
dianedouglas <diane.douglas1@gmail.com>
2017-02-27 13:36:41 -0500

    Add preview display and styling to home page with buttons for my places and new place shown when logged in.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,17 @@ class WelcomeController < ApplicationController
   skip_before_action :authorize
 
   def index
-    # @thing = ENV["test_thing"]
+    @random_place = Place.first(:offset => rand(Place.count))
+    @user = User.find(@random_place.user_id)
+    @url = 'http://api.openweathermap.org/data/2.5/weather?q=' << @random_place.location << '&units=imperial&APPID=' << ENV['WEATHER_API_KEY']
+    uri = URI(URI.encode(@url))
+    response = Net::HTTP.get(uri)
+    @json = JSON.parse(response)
+    if @json["cod"] == 200
+      @icon_url = 'http://openweathermap.org/img/w/' << @json['weather'][0]['icon'] << '.png'
+      @description = @json['weather'][0]['description']
+      @temperature = @json['main']['temp']
+    end
+
   end
 end
