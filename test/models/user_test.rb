2228a53c18016fb30dc26b1a9251cc59cbeb3a24
dianedouglas <diane.douglas1@gmail.com>
2017-02-28 15:05:24 -0500

    Add user model test to show users have unique names.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+  test "user need unique name" do
+    user = User.new(name: users(:one).name,
+    password: 'secret',
+    password_confirmation: 'secret' )
+    assert user.invalid?
+    assert_equal ["has already been taken"], user.errors[:name]
+  end
 end
